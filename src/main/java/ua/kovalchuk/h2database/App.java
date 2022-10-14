986cd9b8a7f6ca3aef11374768e21be60d36d186
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-14 16:35:15 +0300

    Rename main class

--- src/main/java/ua/kovalchuk/h2database/App.java
+++ src/main/java/ua/kovalchuk/h2database/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWithH2DatabaseApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootWithH2DatabaseApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 
 }
