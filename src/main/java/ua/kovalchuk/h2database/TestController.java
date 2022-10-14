0f0070076271a116e7a366a25aed34130689511a
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-10-14 16:37:37 +0300

    Polish

--- src/main/java/ua/kovalchuk/h2database/TestController.java
+++ src/main/java/ua/kovalchuk/h2database/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("ping")
-    public String test(){
+    public String test() {
         return "pong";
     }
 }
